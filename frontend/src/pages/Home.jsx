0d9783f3f5a8f53f19cc392c36f9841cadb9c4c3
Vinay14Newbie <wjust1984@gmail.com>
2025-03-15 23:45:42 +0530

    trying to solve bug

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import { CouponCard } from "../components/CouponCard";
-import { Header } from "../components/Header";
+import { CouponCard } from "../Components/CouponCard";
+import { Header } from "../Components/Header";
 export const Home = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-200 p-4">
