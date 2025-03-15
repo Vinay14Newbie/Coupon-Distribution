1c979f5d6588310cf772d5620ca28347fa289896
Vinay14Newbie <wjust1984@gmail.com>
2025-03-15 23:29:45 +0530

    changes in frontend

--- frontend/src/Components/CouponCard.jsx
+++ frontend/src/Components/CouponCard.jsx
@@ -1,4 +1,27 @@
-export const CouponCard = ({ coupon, message, onClaimCoupon }) => {
+import { useState } from "react";
+import { claimCoupon } from "../utils/api";
+
+export const CouponCard = () => {
+  const [coupon, setCoupon] = useState("");
+  const [message, setMessage] = useState("");
+  const handleSubmit = async () => {
+    console.log("Clicked claim coupon");
+    try {
+      const res = await claimCoupon();
+      console.log("Res ", res.message);
+
+      if (res.success) {
+        setCoupon(res.coupon);
+        setMessage("");
+      } else {
+        setCoupon("");
+        setMessage(res.message);
+      }
+    } catch (error) {
+      setMessage("Failed to claim coupon");
+    }
+  };
+
   return (
     <div className="p-4 border rounded-md shadow-md bg-gray-100 flex justify-center items-center flex-col">
       {coupon ? (
@@ -7,7 +30,7 @@ export const CouponCard = ({ coupon, message, onClaimCoupon }) => {
         <p className="text-red-500">{message}</p>
       )}
       <button
-        onClick={onClaimCoupon}
+        onClick={handleSubmit}
         className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
       >
         Claim Coupon
