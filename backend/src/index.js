c15e9c120a99e271fa18d72604849e594f6da862
Vinay14Newbie <wjust1984@gmail.com>
2025-03-16 08:11:57 +0530

    i

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,14 +36,14 @@ app.use(cors(corsOptions));
 //   })
 // );
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   next();
+// });
 
 app.use('/api/coupons', couponRouter);
 
