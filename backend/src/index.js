7cc0c43f9bff8c1a37bdc53137f6ea76445526b6
Vinay14Newbie <wjust1984@gmail.com>
2025-03-16 08:52:21 +0530

    Problem solved

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,6 @@ import couponRouter from './routers/couponRouter.js';
 const app = express();
 
 // middleware
-// app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
@@ -19,35 +18,6 @@ const corsOptions = {
   credentials: true
 };
 app.use(cors(corsOptions));
-
-// app.use(
-//   cors({
-//     origin: '*',
-//     origin: (origin, callback) => {
-//       if (
-//         !origin ||
-//         origin.includes('https://coupon-distribution-t01c.onrender.com') || // backend
-//         origin.includes('https://coupon-distribution-9uhj.vercel.app') || // frontend on Vercel
-//         origin.startsWith('http://localhost')
-//       ) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error('Not allowed by CORS'));
-//       }
-//     },
-//     allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
-//     credentials: true
-//   })
-// );
-
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   );
-//   next();
-// });
 
 app.use('/api/coupons', couponRouter);
 
