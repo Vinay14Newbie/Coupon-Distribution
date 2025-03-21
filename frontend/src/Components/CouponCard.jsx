c73be75b7a2936710056efaddd73a54ef2228fd5
Vinay14Newbie <wjust1984@gmail.com>
2025-03-21 18:04:00 +0530

    Added claiming message while claming the coupon for user satisfaction

--- frontend/src/Components/CouponCard.jsx
+++ frontend/src/Components/CouponCard.jsx
@@ -4,7 +4,10 @@ import { claimCoupon } from "../utils/api";
 export const CouponCard = () => {
   const [coupon, setCoupon] = useState("");
   const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
   const handleSubmit = async () => {
+    setLoading(true);
     console.log("Clicked claim coupon");
     try {
       const res = await claimCoupon();
@@ -20,20 +23,22 @@ export const CouponCard = () => {
     } catch (error) {
       setMessage("Failed to claim coupon");
     }
+    setLoading(false);
   };
 
   return (
-    <div className="p-4 border rounded-md shadow-md bg-gray-100 flex justify-center items-center flex-col">
+    <div className="p-4 w-[400px] h-[150px] border rounded-md shadow-md bg-gray-100 flex justify-center items-center flex-col">
       {coupon ? (
         <p className="text-green-600 text-lg">Coupon Code: {coupon}</p>
       ) : (
-        <p className="text-red-500">{message}</p>
+        <p className="text-red-500 text-center">{message}</p>
       )}
       <button
         onClick={handleSubmit}
-        className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+        className="mt-4 w-[140px] bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+        disabled={loading}
       >
-        Claim Coupon
+        {loading ? "Claiming..." : "Claim Coupon"}
       </button>
     </div>
   );
