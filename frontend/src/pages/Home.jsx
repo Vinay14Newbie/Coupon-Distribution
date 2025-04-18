278d1fca06fcfb09e7fcf334d4f5c053db7f6f62
Vinay14Newbie <wjust1984@gmail.com>
2025-04-18 16:43:26 +0530

    Added note for using app

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,15 @@ import { CouponCard } from "../Components/CouponCard";
 import { Header } from "../Components/Header";
 export const Home = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-200 p-4">
-      <Header />
-      <CouponCard />
+    <div>
+      <div className="w-full text-gray-700 bg-gray-300 py-2 text-center absolute">
+        Note: The app may take some time to respond as the backend is deployed
+        on Render's free tier, which can cause delays due to inactivity..:)
+      </div>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-200 p-4">
+        <Header />
+        <CouponCard />
+      </div>
     </div>
   );
 };
